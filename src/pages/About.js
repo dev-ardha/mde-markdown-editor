a1d5827030d0acc52f81f878bcb5619e2653d3b3
dev-ardha <ardhaydhtm@gmail.com>
2020-06-07 11:18:29 +0700

    Add react helmet

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Styled from '@emotion/styled'
+import { Helmet } from 'react-helmet'
 
 const About = ()=> {
     return(
         <AboutStyled>
+            <Helmet>
+                <title>My MDE - About</title>
+                <meta name="description" content="A simple markdown editor with live preview built with React.js" />
+                <meta name="keyword" content="markdown, ardha, dev.ardha, markdown editor, text editor, markdown preview, react markdown, my mde, mde, react markdown editor, react markdown preview" />
+                <title>React Markdown Editor</title>
+                <meta name="author" content="dev.ardha"/>
+                <meta name="robots" content="index,follow"/>
+                <meta content='@ardhaydhtm' name='twitter:site'/>
+                <meta content='@ardhaydhtm' name='twitter:creator'/>
+            </Helmet>
             <div className="content">
                 <h1>About</h1>
-                <p>My MDE is an Online Markdown Editor that can help you to write and preview a markdown. You can also download the markdown file by clicking on the download button. This website is built on React, and its open source. If you want to contribute to this project, you can visit my github page <a href="https://github.com/dev-ardha/mymde-markdown-editor">here</a></p>
+                <p>My MDE is an Online Markdown Editor that can help you to write and preview a markdown. You can also download the markdown file by clicking the download button. This website is built on React, and its open source. If you want to contribute to this project, you can visit my github page <a href="https://github.com/dev-ardha/mymde-markdown-editor">here</a></p>
                 <p></p>
                 <p>Developed by <a href="">dev.ardha</a></p>
             </div>
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import Hero from '../components/Hero';
 import Styled from '@emotion/styled'
 import Editor from '../components/Editor';
+import { Helmet } from 'react-helmet'
 
 const Index = ()=> {
     return(
         <IndexStyled>
+            <Helmet>
+                <title>My MDE - React Markdown Editor</title>
+            </Helmet>
             <Hero/>
             <div className="content">
                 <Editor/>
