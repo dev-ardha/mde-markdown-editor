f3f170b53b72cf0b081f4947ef58b0ae1ba42251
dev-ardha <ardhaydhtm@gmail.com>
2020-06-08 07:59:11 +0700

    Fix favicon problem

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const InterpolateHtmlPlugin = require('interpolate-html-plugin');
 
 
 module.exports = {
